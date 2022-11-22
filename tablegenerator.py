0632b0ea35334df101d7c6d7e55f64be0216e2fb
shashank <shashank944533@gmail.com>
2022-11-22 07:29:10 +0530

    First commit

---
+++ tablegenerator.py
@@ -0,0 +1,6 @@
+from tabulate import tabulate
+data = [["Name", "Place", "Gender"], ["Aman", "New Delhi", "Male"], ["Hritika", "New Delhi", "Female"], ["Krishna", "UP", "Male"], ["Arun","karnataka","shwmale"]]
+print(tabulate(data))
+print(tabulate(data, headers="firstrow"))
+print(tabulate(data, headers='firstrow', tablefmt='grid'))
+print(tabulate(data, headers='firstrow', tablefmt='fancy_grid'))
