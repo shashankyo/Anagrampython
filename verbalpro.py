e9de30a340e26a7f84feff46781c88ce2bf710b7
shashank <shashank944533@gmail.com>
2022-11-22 07:30:56 +0530

    first commit

---
+++ verbalpro.py
@@ -0,0 +1,16 @@
+
+#youtuber = "Kylie YOUNG"
+
+
+# print("subscribe to "+ youtuber)
+# print("subscribe to {}" .format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input ("Adjective: ")
+verb1 = input ("verb: ")
+verb2 = input ("verb :")
+famous_person = input ("famous person :")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because  i love to {verb1} . Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
